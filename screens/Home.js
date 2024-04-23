7f9b09dd9d109164daffd24871d1f786da210156
Gibson <114831858+GibsonGYIOVdguisdo@users.noreply.github.com>
2024-04-23 12:11:39 +0100

    Added learning streak card

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 import {
   StyleSheet,
@@ -8,21 +8,20 @@ import {
   Platform,
   Text,
   ScrollView,
-  Button,
-  TouchableOpacity,
-} from "react-native";
+  TouchableOpacity
+} from 'react-native'
 
-function HopBackInCard(amountToReview, amountToLearn, amountLearnt) {
-
+function HopBackInCard (amountToReview, amountToLearn, amountLearnt) {
   return (
     <View style={styles.cardContainer}>
       <View style={styles.card}>
-        <Text style={styles.cardText}>Hop back in</Text>
-        <View style={styles.hopBackInCardDivide}>
+        <Text style={styles.cardText}>Hop Back In</Text>
+          <View style={{marginTop:20}}></View>
+        <View style={styles.cardDivide1}>
           <View>
             <Text style={styles.cardSubText}>
               {amountToReview}
-              {"\n"}to review
+              {'\n'}to review
             </Text>
             <TouchableOpacity style={styles.cardButton}>
               <Text style={styles.cardButtonText}>Review</Text>
@@ -31,7 +30,7 @@ function HopBackInCard(amountToReview, amountToLearn, amountLearnt) {
           <View>
             <Text style={styles.cardSubText}>
               {amountLearnt}/{amountToLearn}
-              {"\n"}learnt
+              {'\n'}learnt
             </Text>
             <TouchableOpacity style={styles.cardButton}>
               <Text style={styles.cardButtonText}>Learn</Text>
@@ -40,89 +39,114 @@ function HopBackInCard(amountToReview, amountToLearn, amountLearnt) {
         </View>
       </View>
     </View>
-  );
+  )
 }
 
-function ProgressCard(AllTimeWordCount, TodaysWordCount, AllTimeMinutes, TodaysMinutes){
-  
+function LearningStreakCard (currentLearningStreak, bestLearningStreak) {
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.card}>
+        <Text style={styles.cardText}>Learning Steak</Text>
+        <View style={{marginTop: 28}}></View>
+        <View style={styles.cardDivide1}>
+          <View style={{alignItems:"center"}}>
+            <Text style={styles.cardSubText}>Current streak</Text>
+            <Text style={styles.cardBigText}>{currentLearningStreak}</Text>
+          </View>
+          <View style={{alignItems:"center"}}>
+            <Text style={styles.cardSubText}>Best streak</Text>
+            <Text style={styles.cardBigText}>{bestLearningStreak}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
 
-function LearningStreakCard(currentLearningStreak, bestLearningStreak){
+function ProgressCard (
+  AllTimeWordCount,
+  TodaysWordCount,
+  AllTimeMinutes,
+  TodaysMinutes
+) {}
 
-}
-
-function Home({ amountToReview, amountToLearn, amountLearnt }) {
+function Home ({ amountToReview, amountToLearn, amountLearnt }) {
   return (
     <View style={styles.body}>
       <Header />
       <ScrollView style={styles.cardView}>
         {HopBackInCard(amountToReview, amountToLearn, amountLearnt)}
+        {LearningStreakCard(12, 15)}
+        {ProgressCard()}
       </ScrollView>
       <Footer />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   body: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     flex: 1,
-    backgroundColor: "#121212",
+    backgroundColor: '#121212'
   },
   cardView: {
-    paddingTop: 33,
-    width: "100%",
-    height: "auto",
-    flexDirection: "column",
-    overflow: "scroll",
+    width: '100%',
+    height: 'auto',
+    flexDirection: 'column',
+    overflow: 'scroll'
   },
 
   card: {
-    width: "90%",
+    width: '90%',
     height: 180,
-    backgroundColor: "#282828",
-    borderRadius: 29,
+    backgroundColor: '#282828',
+    borderRadius: 29
   },
   cardText: {
     height: 27,
     marginTop: 15,
     fontSize: 18,
-    color: "white",
-    textAlign: "center",
-    fontWeight: "600",
-    fontFamily: "Inter",
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: '600',
+    fontFamily: 'Inter'
+  },
+  cardBigText: {
+    color: 'white',
+    fontSize: 40
   },
   cardSubText: {
-    marginTop: 15,
     fontSize: 16,
-    color: "white",
-    textAlign: "center",
-    fontWeight: "400",
-    fontFamily: "Inter",
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: '400',
+    fontFamily: 'Inter'
   },
   cardContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
+
+    marginTop: 33,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
   },
   cardButton: {
     width: 136,
     height: 53,
     borderRadius: 12,
-    backgroundColor: "#A0D39C",
-    alignItems: "center",
-    justifyContent: "space-around",
+    backgroundColor: '#A0D39C',
+    alignItems: 'center',
+    justifyContent: 'space-around'
   },
   cardButtonText: {
     fontSize: 16,
-    fontFamily: "Inter",
+    fontFamily: 'Inter'
   },
-  hopBackInCardDivide: {
-    width: "100%",
-    marginTop: 1,
-    justifyContent: "space-around",
-    flexDirection: "row",
-  },
-});
+  cardDivide1: {
+    width: '100%',
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  }
+})
 
-export default Home;
+export default Home
