c3321dfe0c7aaf9e0e8d028145aa81d73f9577da
Gibson <Gibson570@hotmail.com>
2024-05-10 13:18:03 +0100

    Created functions for learning

--- utils/WordLearning.js
+++ utils/WordLearning.js
@@ -3,22 +3,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 function IsWordLearnt(word) {
   const storedVal = AsyncStorage.getItem('words_german_progress_' + word);
   if (storedVal) {
-    return storedVal > 0;
+    return parseInt(storedVal) > 0;
   }
   return false;
 }
 
 function LearnWord(word) {
   let date = new Date().getDate() + 1;
-  AsyncStorage.setItem('words_german_progress_' + word, 1);
-  AsyncStorage.setItem('words_german_lastGap_' + word, 1);
-  AsyncStorage.setItem('words_german_nextReview_' + word, date);
+  AsyncStorage.setItem('words_german_progress_' + word, '1');
+  AsyncStorage.setItem('words_german_lastGap_' + word, '1');
+  AsyncStorage.setItem('words_german_nextReview_' + word, Date.toString(date));
 }
 
 function GetReviewGap(word) {
   const storedVal = AsyncStorage.getItem('words_german_lastGap_' + word);
   if (storedVal) {
-    return storedVal;
+    return parseInt(storedVal);
   }
   return 0;
 }
@@ -26,7 +26,7 @@ function GetReviewGap(word) {
 function GetReviewDate(word) {
   const storedVal = AsyncStorage.getItem('words_german_nextReview_' + word);
   if (storedVal) {
-    return storedVal;
+    return Date.parse(storedVal);
   }
   return new Date();
 }
