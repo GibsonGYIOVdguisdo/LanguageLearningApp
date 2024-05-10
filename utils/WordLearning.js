a174aab334b29aaba3c231dba4379a0a83ce2445
Gibson <Gibson570@hotmail.com>
2024-05-10 13:17:52 +0100

    Created functions for learning

--- utils/WordLearning.js
+++ utils/WordLearning.js
@@ -1,14 +1,33 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function IsWordLearnt(word) {
-  if (!AsyncStorage.getItem('words_german_' + word)) {
-  }
-  return false;
-}
-function SetWordLearnt(word) {
-  if (!AsyncStorage.getItem('words_german_' + word)) {
+  const storedVal = AsyncStorage.getItem('words_german_progress_' + word);
+  if (storedVal) {
+    return storedVal > 0;
   }
   return false;
 }
 
-export default IsWordLearnt;
+function LearnWord(word) {
+  let date = new Date().getDate() + 1;
+  AsyncStorage.setItem('words_german_progress_' + word, 1);
+  AsyncStorage.setItem('words_german_lastGap_' + word, 1);
+  AsyncStorage.setItem('words_german_nextReview_' + word, date);
+}
+
+function GetReviewGap(word) {
+  const storedVal = AsyncStorage.getItem('words_german_lastGap_' + word);
+  if (storedVal) {
+    return storedVal;
+  }
+  return 0;
+}
+
+function GetReviewDate(word) {
+  const storedVal = AsyncStorage.getItem('words_german_nextReview_' + word);
+  if (storedVal) {
+    return storedVal;
+  }
+  return new Date();
+}
+export default { IsWordLearnt, LearnWord, GetReviewDate, GetReviewGap };
