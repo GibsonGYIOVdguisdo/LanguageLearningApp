754892d1cc03cbc796ec760e0098ab659c501123
Gibson <Gibson570@hotmail.com>
2024-05-11 16:13:39 +0100

    Made new functions for course interactions

---
+++ utils/CourseInteraction.js
@@ -0,0 +1,38 @@
+const courses = {
+  'German': require('../languageCourses/German.json')
+};
+
+function GetCourse(courseName) {
+  return courses[courseName][courseName];
+}
+
+function GetAllSections(courseName) {
+  const sections = Object.keys(GetCourse(courseName));
+  return sections;
+}
+
+function GetAllWords(courseName, section) {
+  if (section) {
+    const courseContent = GetCourse(courseName);
+    return Object.keys(courseContent[section]);
+  }
+  let words = [];
+  for (section of GetAllSections(courseName)) {
+    words.concat(GetAllWords(courseName, section));
+  }
+  return words;
+}
+
+function GetAllWordPairs(courseName, section) {
+  const courseContent = GetCourse(courseName);
+  if (section) {
+    return courseContent[section];
+  }
+  let words = [];
+  for (section of GetAllSections(courseName)) {
+    words.concat(GetAllWords(courseName, section));
+  }
+  return words;
+}
+
+export { GetCourse, GetAllWordPairs, GetAllWords, GetAllSections };
